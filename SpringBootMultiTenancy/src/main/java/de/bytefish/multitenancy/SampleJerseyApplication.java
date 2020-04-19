2260e6dd03f56a54f52339b975f3356f751f34a0
Philipp Wagner <pwag86@googlemail.com>
2020-04-19 15:31:02 +0200

    Adding LICENSE Header

--- SpringBootMultiTenancy/src/main/java/de/bytefish/multitenancy/SampleJerseyApplication.java
+++ SpringBootMultiTenancy/src/main/java/de/bytefish/multitenancy/SampleJerseyApplication.java
@@ -1,3 +1,6 @@
+// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package de.bytefish.multitenancy;
 
 import com.zaxxer.hikari.HikariDataSource;
