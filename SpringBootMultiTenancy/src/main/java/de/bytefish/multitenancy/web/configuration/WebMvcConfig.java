7cc15004ca7ce4ba13cf1bccb31bdf9fb7948886
Philipp Wagner <pwag86@googlemail.com>
2020-04-27 09:20:36 +0200

    Adding Copyrights to clarify License

--- SpringBootMultiTenancy/src/main/java/de/bytefish/multitenancy/web/configuration/WebMvcConfig.java
+++ SpringBootMultiTenancy/src/main/java/de/bytefish/multitenancy/web/configuration/WebMvcConfig.java
@@ -3,11 +3,9 @@
 
 package de.bytefish.multitenancy.web.configuration;
 
-
 import de.bytefish.multitenancy.web.interceptors.TenantNameInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
--- SpringBootMultiTenancy/src/main/java/de/bytefish/multitenancy/web/controllers/CustomerController.java
+++ SpringBootMultiTenancy/src/main/java/de/bytefish/multitenancy/web/controllers/CustomerController.java
@@ -1,3 +1,6 @@
+// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package de.bytefish.multitenancy.web.controllers;
 
 import de.bytefish.multitenancy.model.Customer;
