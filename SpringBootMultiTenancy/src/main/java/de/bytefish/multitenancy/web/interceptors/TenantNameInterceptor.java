70c837faf45f5c3083b765153e6a023652679a83
Philipp Wagner <pwag86@googlemail.com>
2020-04-29 17:49:46 +0200

    Set the Tenant as null in afterCompletion Handler
    Let's respect the RequestScope and null out the Tenant once the Request
    is finished.

--- SpringBootMultiTenancy/src/main/java/de/bytefish/multitenancy/web/interceptors/TenantNameInterceptor.java
+++ SpringBootMultiTenancy/src/main/java/de/bytefish/multitenancy/web/interceptors/TenantNameInterceptor.java
@@ -13,13 +13,25 @@ public class TenantNameInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        // Implement your logic to extract the Tenant Name here. Another way would be to
+        // parse a JWT and extract the Tenant Name from the Claims in the Token. In the
+        // example code we are just extracting a Header value:
         String tenantName = request.getHeader("X-TenantID");
 
-        // Always set the Tenant Name, so we avoid leaking Tenants
-        // between Threads in case, no Tenant is given:
+        // Always set the Tenant Name, so we avoid leaking Tenants between Threads even in the scenario, when no
+        // Tenant is given. I do this because if somehow the afterCompletion Handler isn't called the Tenant Name
+        // could still be persisted within the ThreadLocal:
         ThreadLocalStorage.setTenantName(tenantName);
 
         return true;
     }
 
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+        // After completing the request, make sure to erase the Tenant from the current Thread. It's
+        // because Spring may reuse the Thread in the Thread Pool and you don't want to leak this
+        // information:
+        ThreadLocalStorage.setTenantName(null);
+    }
 }
