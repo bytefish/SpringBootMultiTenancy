9476ef5767bbf71b624f9e9e4a07cccef644486f
Philipp Wagner <pwag86@googlemail.com>
2020-04-29 18:00:21 +0200

    Set Sane Defaults for the ThreadPool to avoid Blocking

--- SpringBootMultiTenancy/src/main/java/de/bytefish/multitenancy/async/AsyncConfig.java
+++ SpringBootMultiTenancy/src/main/java/de/bytefish/multitenancy/async/AsyncConfig.java
@@ -16,6 +16,9 @@ public class AsyncConfig extends AsyncConfigurerSupport {
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 
+        executor.setCorePoolSize(7);
+        executor.setMaxPoolSize(42);
+        executor.setQueueCapacity(11);
         executor.setThreadNamePrefix("TenantAwareTaskExecutor-");
         executor.setTaskDecorator(new TenantAwareTaskDecorator());
         executor.initialize();
